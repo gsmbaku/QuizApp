0e6492b286085eb23d8a22cdea53dd60c58de56d
gsmbaku <gsmbaku1@gmail.com>
2015-08-19 09:05:15 -0500

    Start up

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  	
+  	var answer = "";
+  	var phase1 = function(){
+  	$("#q1A1").on('click', function(event) {
+  		event.preventDefault();
+  		answer = "Deion Sanders";
+  		$("#view").html('Deion Sanders');
+  	});
+  	$("#q1A2").on('click', function(event) {
+  		event.preventDefault();
+  		answer = "Reggie Bush";
+  		$("#view").html('Reggie Bush');
+  	});
+  	$("#q1A3").on('click', function(event) {
+  		event.preventDefault();
+  		answer = "Demarco Murray";
+  		$("#view").html('Demarco Murray');
+  	});  	
+
+$("#submit").on('click', function(event) {
+  		event.preventDefault();
+  	if(answer == )
+	}); 
+
+};
+
+});
